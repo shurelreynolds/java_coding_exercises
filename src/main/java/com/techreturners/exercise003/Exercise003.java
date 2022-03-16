9b81754ebdb91473d2283b57f96f454558c90441
Shurel Reynolds <shurel_reynolds@yahoo.com>
2022-03-16 10:09:43 +0000

    Exercise003 done

--- src/main/java/com/techreturners/exercise003/Exercise003.java
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -1,13 +1,62 @@
 package com.techreturners.exercise003;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
+
 public class Exercise003 {
+    List<Item> iceCreamList = new ArrayList<>();
+
+    public Exercise003() {
+        iceCreamList.add(new Item(0, "Pistachio"));
+        iceCreamList.add(new Item(1, "Raspberry Ripple"));
+        iceCreamList.add(new Item(2, "Vanilla"));
+        iceCreamList.add(new Item(3, "Mint Chocolate Chip"));
+        iceCreamList.add(new Item(4, "Chocolate"));
+        iceCreamList.add(new Item(5, "Mango Sorbet"));
+    }
 
     int getIceCreamCode(String iceCreamFlavour) {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        OptionalInt iceCream = OptionalInt.of(iceCreamList.stream().filter(i -> i.getName().equals(iceCreamFlavour)).mapToInt(i -> i.getId()).findFirst()
+                .orElseThrow(()->new NoSuchIceCreamFlavourException(iceCreamFlavour)));
+
+        return iceCream.getAsInt();
+
     }
 
     String[] iceCreamFlavours() {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        List<String> flavourList = iceCreamList.stream().map(i -> i.getName()).collect(Collectors.toList());
+        String[] flavours = new String[flavourList.size()];
+        flavourList.toArray(flavours);
+        return flavours;
     }
+
+    static class Item implements Comparable<Item> {
+        final int id;
+        final String name;
+
+        Item(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        String getName() {
+            return this.name;
+        }
+
+        int getId() {
+            return this.id;
+        }
+
+        @Override
+        public int compareTo(Item item) {
+              int compareId=item.getId();
+
+               return this.id - compareId;
+            }
+        }
+
 
 }
--- src/test/java/com/techreturners/exercise003/Exercise003Test.java
+++ src/test/java/com/techreturners/exercise003/Exercise003Test.java
@@ -1,10 +1,14 @@
 package com.techreturners.exercise003;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertArrayEquals;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.Ignore;
-
-import static org.junit.Assert.assertEquals;
 
 public class Exercise003Test {
 
@@ -32,23 +36,41 @@ public class Exercise003Test {
         assertEquals(expected, ex003.getIceCreamCode(iceCreamFlavour));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkGetIceCreamCodeForRaspberryRipple() {
         String iceCreamFlavour = "Raspberry Ripple";
         int expected = 1;
 
         assertEquals(expected, ex003.getIceCreamCode(iceCreamFlavour));
+
+        expected = 9;
+
+        assertNotEquals(expected, ex003.getIceCreamCode(iceCreamFlavour));
+
+
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkPickMultipleIceCreamFlavours() {
 
         String[] expected = { "Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet" };
+        String []flavours=ex003.iceCreamFlavours();
 
-        assertEquals(expected, ex003.iceCreamFlavours());
+        assertArrayEquals(expected, flavours);
+        //asserts that flavour is populated
+        assertNotEquals(0, flavours.length);
     }
 
+    @Test
+    public void testNoSuchIceCreamFlavourForCode() {
+        boolean thrown = false;
+        String iceCreamFlavour = "Guava";
+        try {
+            ex003.getIceCreamCode(iceCreamFlavour);
+        } catch (NoSuchIceCreamFlavourException e) {
+            thrown = true;
+        }
 
+        assertTrue(thrown);
+    }
 }

6551b70e3cb350535a0d5e91fddb49a5f03ac96a
Shurel Reynolds <shurel_reynolds@yahoo.com>
2022-03-17 21:09:11 +0000

    Exercise005 done

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -1,9 +1,33 @@
 package com.techreturners.exercise005;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import static java.util.stream.Collectors.*;
+
 public class Exercise005 {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        if (input == null || input.matches("\\d+"))
+            return false;
+
+        input = input.toLowerCase();
+
+        //check for ascii character
+        if(input.matches("^[\\p{ASCII}]"))
+            return false;
+
+        for (char c = 'a'; c < 'z'; c++) {
+
+            if (!input.contains(c + ""))
+                return false;
+        }
+
+        return true;
     }
 
 }
--- src/test/java/com/techreturners/exercise005/Exercise005Test.java
+++ src/test/java/com/techreturners/exercise005/Exercise005Test.java
@@ -1,7 +1,6 @@
 package com.techreturners.exercise005;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
@@ -21,52 +20,55 @@ public class Exercise005Test {
         assertFalse(ex005.isPangram(""));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkPerfectLowerCaseSentenceIsAPangram() {
         assertTrue(ex005.isPangram("abcdefghijklmnopqrstuvwxyz"));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkPerfectUpperCaseSentenceIsAPangram() {
         assertTrue(ex005.isPangram("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkSentenceWithLowerCaseIsAPangram() {
         assertTrue(ex005.isPangram("the quick brown fox jumps over the lazy dog"));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkSentenceWithMixedUpperAndLowerCaseIsAPangram() {
         assertTrue(ex005.isPangram("The Five boXing wiZards Jump QuicklY"));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkSentenceWithMissingLetterXIsNotAPangram() {
         assertFalse(ex005.isPangram("the quick brown fo jumps over the lazy dog"));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkSentenceWithMissingLetterHIsNotAPangram() {
         assertFalse(ex005.isPangram("five boxing wizards jump quickly at it"));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkSentenceWithUnderscoresIsAPangram() {
         assertTrue(ex005.isPangram("the_quick_brown_fox_jumps_over_the_lazy_dog"));
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkSentenceWithNumbersIsAPangram() {
         assertTrue(ex005.isPangram("the 1 quick brown fox jumps over the 2 lazy dogs"));
     }
+
+    @Test
+    public void checkNumbersOnlyIsAPangram() {
+        assertFalse(ex005.isPangram("357986315789007"));
+    }
+
+    @Test
+    public void checkSentenceWithAsciiIsAPangram() {
+        assertFalse(ex005.isPangram("the 1 qu^ck brown fox jumps over the 2 lazy dogs"));
+    }
+
 
 }
